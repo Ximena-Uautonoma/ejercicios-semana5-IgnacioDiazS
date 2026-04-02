40c79cce0a73e55bfb551e531dfa8fcc24970e26
IgnacioDiazS <ignacio.diaz12@cloud.uautonoma.cl>
2026-04-02 19:47:20 +0000

    cajeor

--- Semana5/Ejercicio3.py
+++ Semana5/Ejercicio3.py
@@ -2,6 +2,12 @@
 Un cajero permite retirar solo montos mayores a 0 y múltiplos de 10.
 Solicita el monto hasta que sea válido y luego muestra "Retiro exitoso".'''
 
-
-
-
+print("Cajero automatico")
+entrar = 0
+input("Ingresa 0 para entrar")
+while entrar == 0:
+    a = int(input("Ingrese un monto para retirar:"))
+    if (0 < a) and (a % 10 == 0) : 
+        print ("Retiro exitoso")
+    else :
+        print ("Ingrese un monto valido")   
