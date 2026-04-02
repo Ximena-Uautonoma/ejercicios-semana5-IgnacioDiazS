8542716b5ee58e926c8d6ddd9e8c8c981a6d8566
IgnacioDiazS <ignacio.diaz12@cloud.uautonoma.cl>
2026-04-02 19:00:08 +0000

    if elif temperatura

--- Semana5/Ejercicio2.py
+++ Semana5/Ejercicio2.py
@@ -6,7 +6,13 @@ Un sistema de climatización clasifica:
 Solicita la temperatura e indica la clasificación correspondiente.
 '''
 
-
+x = int(input("Ingresa la temperatura para clasificarla:"))
+if x<=10 :
+    print("La temperatura es fria")
+elif 10<x<=25:
+    print("La temperatura es templada")
+elif x>=26:
+    print("La temperatura es caluorsa")
 
 
 
